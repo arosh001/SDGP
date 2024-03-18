4549725df8c9541f0bc32af54e88c475111d2e82
HimayaMedni <himayamedini2001@gmail.com>
2024-03-18 10:42:47 +0530

    Update Privacy.js
    Add content

--- frontend/Privacy.js
+++ frontend/Privacy.js
@@ -26,3 +26,38 @@ export default function App() {
         console.log("Please Agree to Privacy Policies!")
       }
     };
+
+
+    const styles = useStyles();
+
+  return (
+    <View style={styles.root}>
+      <ImageBackground source={require ('../frontend/assets/Images/BG 2.png')} style={styles.bgImage2}/>
+    
+      <View style={styles.sub_root}>
+        <View style={styles.box1}>
+          <View style={styles.boxTop}>
+            <Text style={styles.topic}>Privacy Policy :</Text>
+          </View>
+          <View style={styles.boxMiddle}>
+            <ScrollView>
+              <Text style={styles.paragraph}>
+                We respect your privacy and are committed to protecting your personal information.
+                Any data collected from you will be used solely for the purpose of providing and improving our services.
+                We assure you that your information will not be shared with third parties without your explicit consent.
+                We implement industry-standard security measures to safeguard your data against unauthorized access and
+                ensure its confidentiality. By using our services, you agree to the collection and use of information
+                in accordance with this policy. Please review our Privacy Policy periodically for any updates.
+                If you have any concerns or questions, feel free to contact us.
+              </Text>
+            </ScrollView>
+          </View>
+          <View style={styles.boxBottom}>
+            <View style={styles.validate}>
+              <TouchableOpacity style={[styles.checkbox, isChecked && styles.checked]} onPress={() => setIsChecked(!isChecked)}>
+                {isChecked && <Text style={styles.checkmark}>✓</Text>}
+              </TouchableOpacity>
+              <Text style={styles.agreeText}>I agree</Text>
+            </View>
+          </View>
+        </View>
