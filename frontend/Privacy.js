871f106d3b88e94dbde4946f488ba7f9a9342e0d
HimayaMedni <himayamedini2001@gmail.com>
2024-03-18 09:59:51 +0530

    Update Privacy.js
    Add new React Native Imports

--- frontend/Privacy.js
+++ frontend/Privacy.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { useFonts } from 'expo-font';
-import { useNavigation } from '@react-navigation/native';
+// React Native Imports
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, useWindowDimensions, ImageBackground, Image } from "react-native";
+import { useFonts } from "expo-font";
+import { useNavigation } from "@react-navigation/native";
+
+// ______________________________Dev Area______________________________ //
 
 
 
