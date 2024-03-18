c434b904a7b5ebd9a860904eb7f89e189a7f2d4b
HimayaMedni <himayamedini2001@gmail.com>
2024-03-18 09:35:59 +0530

    Create Privacy.js
    Validate checkbox function and do navigation

---
+++ frontend/Privacy.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useFonts } from 'expo-font';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Topic': require('./assets/Fonts/Inter/static/Inter-Bold.ttf'),
+    'Inter-Body': require('./assets/Fonts/Inter/static/Inter-Regular.ttf')
+  });
+
+  const [isChecked, setIsChecked] = useState(false);
+
+  const navigation = useNavigation();
+
+  if (!fontsLoaded) {
+    return null;
+  }
