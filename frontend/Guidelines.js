99cff6dee86431db2c9318e4f353bfc87a41a6fa
HimayaMedni <himayamedini2001@gmail.com>
2024-03-19 14:45:09 +0530

    Create Guidelines.js

---
+++ frontend/Guidelines.js
@@ -0,0 +1,8 @@
+// React Native Imports
+import React, { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, useWindowDimensions, ImageBackground, Image } from "react-native";
+import { useFonts } from "expo-font";
+import { useNavigation } from "@react-navigation/native";
+
+// ______________________________Dev Area______________________________ //
