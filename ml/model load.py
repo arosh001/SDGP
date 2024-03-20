b18b81f61435168c888dacb30d37535cbe6a20b7
Sandaru Dharmapriya <148580595+Sandaru-Dharmapriya@users.noreply.github.com>
2024-03-20 19:59:03 +0530

    define the extract bounding box function
    this is a function we can call this function when we want that

--- ml/model load.py
+++ ml/model load.py
@@ -7,3 +7,13 @@ mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
 
 model = load_model("C:\\Users\\sidug\\Downloads\\trained_model (2).h5")
+
+# Define the extract_bounding_box function
+def extract_bounding_box(hand_landmarks, frame_shape):
+    x_values = [landmark.x for landmark in hand_landmarks.landmark]
+    y_values = [landmark.y for landmark in hand_landmarks.landmark]
+
+    x_min = min(x_values) * frame_shape[1]
+    y_min = min(y_values) * frame_shape[0]
+
+    return int(x_min), int(y_min)
