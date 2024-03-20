c28c517b901edb4a9b159acbc2db33f6217adfff
Sandaru Dharmapriya <148580595+Sandaru-Dharmapriya@users.noreply.github.com>
2024-03-20 19:55:30 +0530

    import mediapipe hand module
    import the midea pipe hand module ti detect hand which are in exreact frames

--- ml/model load.py
+++ ml/model load.py
@@ -2,3 +2,6 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from tensorflow.keras.models import load_model
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
