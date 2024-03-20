425ca8d4b1768418a65762e5456c9d3eef8eb615
Sandaru Dharmapriya <148580595+Sandaru-Dharmapriya@users.noreply.github.com>
2024-03-20 19:56:57 +0530

    load the model
    load the pre-train cnn model to predict

--- ml/model load.py
+++ ml/model load.py
@@ -5,3 +5,5 @@ from tensorflow.keras.models import load_model
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5)
+
+model = load_model("C:\\Users\\sidug\\Downloads\\trained_model (2).h5")
