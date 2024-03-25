438e13bdedb30d63d56f250a7bd961d0fbe3a0a9
HimayaMedni <himayamedini2001@gmail.com>
2024-03-25 10:57:07 +0530

    Update SignUp(CRUD).js
    changing styles with add more

--- frontend/SignUp(CRUD).js
+++ frontend/SignUp(CRUD).js
@@ -48,4 +48,15 @@ export default function SignUpScreen({ navigation }) {
             alert("Passwords are different!")
         }
     }
+
+    return <View style={styles.view}>
+    <Text style={{ fontSize: 34, fontWeight: "800", marginBottom: 20 }}>Sign Up</Text>
+    <TextBox placeholder="Email Address" onChangeText={text => handleChange(text, "email")} />
+    <TextBox placeholder="Password" secureTextEntry={true}  onChangeText={text => handleChange(text, "pwd")}/>
+    <TextBox placeholder="Confirme Password" secureTextEntry={true}  onChangeText={text => handleChange(text, "pwd2")}/>
+    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "92%", backgroundColor: '' }}>
+        <Btn onClick={() => SignUp()} title="Sign Up" style={{ width: "48%" }} />
+        <Btn onClick={() => navigation.replace("Login")} title="Login" style={{ width: "48%", backgroundColor: "#344869" }} />
+    </View>
+</View>
 }
