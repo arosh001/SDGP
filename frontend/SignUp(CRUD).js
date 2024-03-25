472cc544e7147c804e8578974b3b741e294e1112
HimayaMedni <himayamedini2001@gmail.com>
2024-03-25 10:49:10 +0530

    Update SignUp(CRUD).js
    Make SingUp function

--- frontend/SignUp(CRUD).js
+++ frontend/SignUp(CRUD).js
@@ -4,3 +4,38 @@ import TextBox from "../components/TextBox"
 import Btn from "../components/Btn"
 import firebase from 'firebase/app';
 import "firebase/auth";
+
+export default function SignUpScreen({ navigation }) {
+
+    const [values, setValues] = useState({
+        email: "",
+        pwd: "",
+        pwd2: ""
+    })
+
+    function handleChange(text, eventName) {
+        setValues(prev => {
+            return {
+                ...prev,
+                [eventName]: text
+            }
+        })
+    }
+
+    function SignUp() {
+
+        const { email, pwd, pwd2 } = values
+
+        if (pwd == pwd2) {
+            firebase.auth().createUserWithEmailAndPassword(email, pwd)
+                .then(() => {
+                })
+                .catch((error) => {
+                    alert(error.message)
+                    // ..
+                });
+        } else {
+            alert("Passwords are different!")
+        }
+    }
+}
