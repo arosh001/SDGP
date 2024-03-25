d516322fd41cdee462f56058ab2fc573a1d67197
HimayaMedni <himayamedini2001@gmail.com>
2024-03-25 10:38:44 +0530

    Update Login(CRUD).js
    Make validations

--- frontend/Login(CRUD).js
+++ frontend/Login(CRUD).js
@@ -43,4 +43,14 @@ export default function Loginscreen({ navigation }) {
                 // ..
             });
     }
+
+    return <View style={styles.view}>
+    <Text style={{ fontSize: 34, fontWeight: "800", marginBottom: 20 }}>Login</Text>
+    <TextBox placeholder="Email Address" onChangeText={text => handleChange(text, "email")} />
+    <TextBox placeholder="Password" onChangeText={text => handleChange(text, "pwd")} secureTextEntry={true} />
+    <View style={{ flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "92%",  }}>
+        <Btn onClick={() => Login()} title="Login" style={{ width: "48%" }} />
+        <Btn onClick={() => navigation.navigate("Sign Up")} title="Sign Up" style={{ width: "48%", backgroundColor: "#344869" }} />
+    </View>
+</View>
 }
